290097d425c695a2e0da43dcb9b6d296f7d8634f
Siddharth Hooda <siddharthhooda@Siddharths-MacBook-Air.local>
2025-03-04 07:06:42 +0530

    test deleted

--- App.tsx
+++ App.tsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import AuthNavigator from "./src/Navigations/AuthNavigator";
 import AppStack from "./src/Navigations/AppStack";
+import Navigation from "./src/Test/TEstStack";
 
 const App = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(false); // Replace with real auth logic
@@ -11,7 +12,8 @@ const App = () => {
     {/* <NavigationContainer>
       {isLoggedIn ? <AppStack /> : <AuthNavigator />}
     </NavigationContainer> */}
-    <AuthNavigator />
+    {/* <AuthNavigator /> */}
+    <Navigation />
     </>
   );
 };
